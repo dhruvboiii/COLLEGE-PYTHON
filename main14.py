150228248804d5474e561ea16e9e87ae4eba1bb5
dhruvpampattiwar <dhruvpampattiwar@gmail.com>
2026-09-23 23:45:31 +0530

    Add files via upload

---
+++ main14.py
@@ -0,0 +1,8 @@
+a=int(input('Enter 1st number: '))
+b=int(input('Enter 2nd number: '))
+if a>b:
+    print(f'{a} is greater than {b}')
+elif a<b:
+    print(f'{b} is greater than {a}')
+else:
+    print(f'{a} is equal to {b}')
