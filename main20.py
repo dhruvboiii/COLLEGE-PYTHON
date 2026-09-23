50aeb837fc7c84044ac024649da9e3af36bed748
dhruvpampattiwar <dhruvpampattiwar@gmail.com>
2026-09-23 23:47:05 +0530

    Add files via upload

---
+++ main20.py
@@ -0,0 +1,11 @@
+a=int(input('Enter your percentage:'))
+if a>=90:
+    print('A')
+elif 70<=a<90:
+    print('B')
+elif 50<=a<70:
+    print('C')
+elif 35<=a<50:
+    print('D')
+else:
+    print('F')
