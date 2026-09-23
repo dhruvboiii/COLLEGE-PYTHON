a1f01c57891532a458072211277e5c4e365c5649
dhruvpampattiwar <dhruvpampattiwar@gmail.com>
2026-09-23 23:46:49 +0530

    Add files via upload

---
+++ main19.py
@@ -0,0 +1,11 @@
+a=int(input('Enter a number:'))
+b=int(input('Enter a number:'))
+c=input('What to do?')
+if 'add' in c:
+    print(a+b)
+elif 'subtract' in c:
+    print(a-b)
+elif 'multiply' in c:
+    print(a*b)
+elif 'divide' in c:
+    print(a/b)            
